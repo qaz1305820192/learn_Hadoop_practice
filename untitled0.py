b11f43736d8a1d6d42de9fa8bf1e3ffc304b3f8b
w jw <32531689+qaz1305820192@users.noreply.github.com>
2020-06-19 20:14:58 +0800

    Update untitled0.py
    查找同一文件下的 多个doc文件 中哪些文件含指定的关键字

--- untitled0.py
+++ untitled0.py
@@ -1,31 +1,131 @@
-# encoding=utf-8
+
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 18 16:39:55 2020
+查找多个doc中的某些关键字，将含有这些关键字的word文件进行列表显示
+@author: 13058
+"""
+
+from win32com import client as wc
+import docx
 import os
-import argparse
-import tqdm
 
-def GetFiles(input,output):
-    if not os.path.isdir(input):
-        print("input dirs not exit")
-        exit(-1)
-    if not os.path.exists(output):os.mkdir(output)
-    return filter(os.path.isfile,(input + '/' + _file for _file in os.listdir(input))),(output + '/' + _file for _file in os.listdir(input))
-def Reg_Aladin(reg_aladin,alts,flo,result):
-    if not os.path.exists(reg_aladin):
-        print("please input reg_aladin location!")
-        exit(-1)
-    os.system("{0} -ref {1} -flo {2} -res {3}".format(reg_aladin,alts,flo,result))
 
+   
+def doSaveAas(doc_path):
+    docx_path = doc_path.replace("doc", "docx")
+    word = wc.Dispatch('Word.Application')
+    doc = word.Documents.Open(doc_path)# 目标路径下的文件
+    doc.SaveAs(docx_path, 12, False, "", True, "", False, False, False, False)# 转化后路径下的文件
+    doc.Close()
+    return docx_path
+
+#获取文件列表
+def pathsDoc(path):
+        path_collection=[]
+        for dirpath,dirnames,filenames in os.walk(path):
+                for file in filenames:
+                        fullpath=os.path.join(dirpath,file)
+                        if fullpath.endswith(".doc"):
+                            path_collection.append(fullpath)
+        return path_collection
+#获取文件列表
+def pathsDocx(path):
+        path_collection=[]
+        for dirpath,dirnames,filenames in os.walk(path):
+                for file in filenames:
+                        fullpath=os.path.join(dirpath,file)
+                        if fullpath.endswith(".docx"):
+                            path_collection.append(fullpath)
+        return path_collection
+
+
+def readDocx(filepath):
+    #获取文档对象
+    file=docx.Document(filepath)
+    content=""
+    for para in file.paragraphs:
+        content=content+para.text
+    return content
+ 
+
+def mathcKey(filepath,strKey):
+    content=readDocx(filepath)
+    if content.find(strKey, 0, len(content))!=-1:
+        return path
+    return ""
+
+
+def resultFile(fileDir,keyWord):
+    path=fileDir
+    #首先把doc文件转化成docx文件
+    for filepath in pathsDoc(path):
+        doSaveAas(filepath)
+    #保存查到的文件
+    result=[]
+    for filepath in pathsDocx(path):
+        #如果匹配到了则保存到result中
+        if mathcKey(filepath,keyWord)!='':
+            result.append(filepath)
+    #删除程序生成的docx文件
+    for filepath in pathsDoc(path):
+        os.remove(filepath+'x')
+    #输出结果
+    result2=[]        
+    for str1 in result:
+        print(str1)
+        #去掉文件结尾的 x
+        result2.append(str1[0:len(str1)-1])
+    return result2        
+    
+from shutil import copyfile     
+import easygui        
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-reg", "--reg_aladin", help="reg_aladin location")
-    parser.add_argument("-ref","--reg_filename",help=" Reference image filename")
-    parser.add_argument("-i", "--input_dirs", help="Input dirs")
-    parser.add_argument("-o", "--output_dirs", help="Output dirs")
-    agrs=parser.parse_args()
-    if agrs.input_dirs==None:
-        print("please use \"python registration.py -h \" for help")
-        exit(-1)
-    inputs,outputs=GetFiles(agrs.input_dirs,agrs.output_dirs)
-    pbar=tqdm.tqdm(zip(inputs,outputs))
-    for i in pbar:
-        Reg_Aladin(agrs.reg_aladin,agrs.reg_filename,i[0],i[-1])
+    #指定查找的目录
+    
+   # doSaveAas("C:\\Users\\13058\\Desktop\\pp\\70邱共和张勇等走私普通货物物品罪二审刑事裁定书.doc")
+    
+    if easygui.ynbox('选择要搜索的文件夹', '亲爱的小彭', ('是', '否')):
+        fileDir=easygui.diropenbox()
+        keyWord=easygui.enterbox("要搜索的关键字",title="要搜索的关键字", default="", strip=True, image=None, root=None)
+        
+        resultfile=resultFile(fileDir,keyWord)
+        if len(resultfile)!=0:
+            if False == os.path.exists(path+"\查找的结果"):
+                os.mkdir(fileDir+'\查找的结果')
+            for file in resultfile:
+                filename=file.split('\\')[len(file.split('\\'))-1]
+                copyfile(file,fileDir+'\查找的结果\\'+filename)
+        else:
+            easygui.msgbox("这个文件里的doc文件没有包含："+keyWord)
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
